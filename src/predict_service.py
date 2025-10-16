3fab925fbee13e090b44a1c4a82b4de14b7923a5
Kasim Al-Said <kasim.alsaid@example.com>
2025-10-16 23:21:06 +0200

    Fix lint error: remove blank line at end of file

--- src/predict_service.py
+++ src/predict_service.py
@@ -59,3 +59,4 @@ def predict(payload: PatientData):
         raise HTTPException(status_code=422, detail=str(ve))
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
